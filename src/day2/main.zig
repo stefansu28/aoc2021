a214560739a02d4baaccfabf2dbb8fd8bd6a911a
Stefan Su <stefansu28@gmail.com>
2021-12-03 00:10:29 -0500

    day2 part 2

--- src/day2/main.zig
+++ src/day2/main.zig
@@ -1,42 +1,50 @@
 const std = @import("std");
 const print = std.debug.print;
 
+fn getWord(str: []u8) []u8 {
+    for (str) |c, n| {
+        if (c == ' ') return str[0..n];
+    }
+
+    return str[0..];
+}
+
 pub fn main() anyerror!void {
-    const allocator = std.heap.page_allocator;
     const flags = std.fs.File.OpenFlags {
         .read = true
     };
-    const filename = std.mem.spanZ(std.os.argv[1]);// std.process.args().nextPosix() orelse "";
+    const filename = std.mem.spanZ(std.os.argv[1]);
     print("filename: {s}\n", .{filename});
     const input = try std.fs.openFileAbsolute(filename, flags);
     defer input.close();
     const reader = input.reader();
 
+    var horizontal: u64 = 0;
+    var depth: u64 = 0;
+    var aim: u64 = 0;
+
     var buffer: [100]u8 = undefined;
     var n: u8 = 0;
-    while (reader.readUntilDelimiterOrEof(buffer[0..], ' ')) |lineOption| {
+    while (reader.readUntilDelimiterOrEof(buffer[0..], '\n')) |lineOption| {
         if (lineOption) |line| {
-            // print("{s}\n", .{line});
-            // var command: [10]u8 = undefined;
-            switch (line) {
-                "forward" => print("movin forward"),
-                else => {}
+            const dir = getWord(line);
+            const amount = try std.fmt.parseInt(u8, getWord(line[dir.len+1..]), 10);
+            print("dir: {s}, amount: {}\n", .{dir, amount});
+            if (std.mem.eql(u8, dir, "forward")) {
+                horizontal += amount;
+                depth += aim*amount;
+            } else if (std.mem.eql(u8, dir, "up")) {
+                aim -= amount;
+            } else if (std.mem.eql(u8, dir, "down")) {
+                aim += amount;
+            } else {
+                unreachable;
             }
-            
         } else break;
     } else |err| {
         print("error: {}\n", .{err});
         unreachable;
     }
-    
-    // var inc: u32 = 0;
-    // var last: u32 = 9999*3;
-    // var n: u32 = 1;
-    // while (n < input.len - 1) {
-    //     const sum = input[n - 1] + input[n] + input[n + 1];
-    //     if (sum > last) inc += 1;
-    //     last = sum;
-    //     n += 1;
-    // }
-    // print("incs: {}", .{inc});
+
+    print("{}\n", .{horizontal * depth});
 }
